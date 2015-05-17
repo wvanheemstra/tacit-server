4a5a82462c550c7761b4ad21c99cca8b91d5ad28
Willem van Heemstra <wvanheemstra@icloud.com>
2015-05-17 16:50:37 +0100

    Updated

--- index.js
+++ index.js
@@ -7,14 +7,38 @@
 * var server = tacitServer.server;
 * //do something with server
 */
-var TacitServer = []; // placeholder object
 
-var express = require('express');
+// See also http://www.sitepoint.com/understanding-module-exports-exports-node-js/
+
+var exports = module.exports = {}; // placeholder object
+
+var express = require('express'),
+path = require('path'),
+mime = require('mime'),
+fs = require('fs'),
+url = require('url'),
+http = require('http'),
+cors = require('cors'),
+runner = require('child_process'),
+morgan = require('morgan'),
+partials = require('express-partials'),
+device = require('./lib/device.js'),
+hash = require('./lib/pass.js').hash,
+redirect = require('express-redirect'),
+bodyParser = require('body-parser'),
+cookieParser = require('cookie-parser'),
+i18n = require('i18n-2'),
+methodOverride = require('method-override'),
+errorHandler = require('errorhandler'),
+sass = require('node-sass'),
+sassMiddleware = require('node-sass-middleware'),
+session = require('express-session'),
+passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy,
+FacebookStrategy = require('passport-facebook').Strategy;
 
 /*
 * SERVER - The Server used for shutdown etc
 * See: https://www.exratione.com/2011/07/running-a-nodejs-server-as-a-service-using-forever/
 */
-var server = TacitServer.server = express();
-
-module.exports = new TacitServer();
+var server = exports.server = express();
